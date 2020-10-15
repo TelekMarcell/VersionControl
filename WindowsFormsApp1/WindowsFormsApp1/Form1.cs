1cd32a1b58cbaf569b763e44a0dbbe835dfeddcb
Marcell Telek <marcell.telek@stud.uni-corvinus.hu>
2020-10-15 13:48:31 +0200

    hhh

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml;
 using WindowsFormsApp1.Entities;
 using WindowsFormsApp1.MnbServiceReference;
 
@@ -19,20 +21,58 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
             FelevArfolyam();
+            Xmlfgv();
+            Tabla();
             dataGridView1.DataSource = Rates;
+            chartRateData.DataSource = Rates;
         }
 
-        private void FelevArfolyam()
-        {
-            var mnbService = new MNBArfolyamServiceSoapClient();
-            var request = new GetExchangeRatesRequestBody()
+            void FelevArfolyam()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
-            };
-            var response = mnbService.GetExchangeRates(request);
-            var result = response.GetExchangeRatesResult;
+                var mnbService = new MNBArfolyamServiceSoapClient();
+                var request = new GetExchangeRatesRequestBody()
+                {
+                    currencyNames = "EUR",
+                    startDate = "2020-01-01",
+                    endDate = "2020-06-30"
+                };
+                var response = mnbService.GetExchangeRates(request);
+                var result = response.GetExchangeRatesResult;
+            }
+
+            void Xmlfgv()
+            {
+                var xml = new XmlDocument();
+                xml.LoadXml(result);
+                foreach (XmlElement element in xml.DocumentElement)
+                {
+                    var rate = new RateData();
+                    Rates.Add(rate);
+                    rate.Date = DateTime.Parse(element.GetAttribute("date"));
+                    var childElement = (XmlElement)element.ChildNodes[0];
+                    rate.Currency = childElement.GetAttribute("curr");
+                    var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                    var value = decimal.Parse(childElement.InnerText);
+                    if (unit != 0)
+                        rate.Value = value / unit;
+                }
+            }
+
+        void Tabla()
+        {
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
+     
     }
 }
+
