5441b47250cd077197e00f74fcc714d5303b9d12
Marcell Telek <marcell.telek@stud.uni-corvinus.hu>
2020-10-25 18:47:42 +0100

    gg

---
+++ task07/task07/Entities/BirthProbalitily.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task07.Entities
+{
+    public class BirthProbalitily
+    {
+        public int BirthYear { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double Probalitily { get; set; }
+    }
+}
---
+++ task07/task07/Entities/DeathProbalitily.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task07.Entities
+{
+    public class DeathProbalitily
+    {
+        public int BirthYear { get; set; }
+        public Gender Gender { get; set; }
+        public double DeathProba { get; set; }
+    }
+}
